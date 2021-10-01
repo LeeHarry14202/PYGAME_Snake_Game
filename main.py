9121919ae46c48a76b8d83d935ea80b3fd5d6a01
LeeHarry14202 <20521912@gm.uit.edu.vn>
2021-10-01 11:58:36 +0700

    Update main.py

--- main.py
+++ main.py
@@ -10,58 +10,90 @@ SCREEN_HEIGHT = 500
 
 screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_WIDTH))
 
+
 class COLOR:
     BLACK = (0, 0, 0)
     WHITE = (255, 255, 255)
     RED = (255, 0, 0)
 
+
 class WORLD(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)
         self.game_active = True
-                # Set fps
+        # Set fps
         self.clock = pygame.time.Clock()
         self.fps_number = 90
+        # Set direction
+        self.direction = 'RIGHT'
+
 
 class SNAKE(object):
     def __init__(self):
         self.x = 0
         self.y = 0
-        WIDTH = 10
-        HEIGHT = 10
-        self.rect = pygame.rect.Rect((self.x, self.y, WIDTH, HEIGHT))
+        self.WIDTH = 100 / 4
+        self.HEIGHT = 100 / 4
+        self.speed = 3
+        self.list_snake = []
+        self.rect = pygame.rect.Rect((self.x, self.y, self.WIDTH, self.HEIGHT))
 
-    def handle_keys(self):
-        key = pygame.key.get_pressed()
-        dist = 1
-        if key[pygame.K_LEFT]:
-           self.rect.move_ip(-1, 0)
-        if key[pygame.K_RIGHT]:
-           self.rect.move_ip(1, 0)
-        if key[pygame.K_UP]:
-           self.rect.move_ip(0, -1)
-        if key[pygame.K_DOWN]:
-           self.rect.move_ip(0, 1)
-
-    def draw(self):
+    def draw(self, x, y):
+        if x != self.x or y != self.y:
+            self.x = x
+            self.y = y
+        self.rect = pygame.rect.Rect((self.x, self.y, self.WIDTH, self.HEIGHT))
         pygame.draw.rect(screen, COLOR.WHITE, self.rect)
+
+    def change_direction(self):
+        if world.direction == 'RIGHT':     
+            self.x += self.speed
+        elif world.direction == 'LEFT':
+            self.x -= self.speed
+        elif world.direction == 'UP':
+            self.y -= self.speed
+        elif world.direction == 'DOWN':
+            self.y += self.speed
+        return self.x, self.y
+
+    def reset_snake(self):
+        if self.x >= SCREEN_WIDTH:
+            self.x = 0
+        elif self.x <= 0:
+            self.x = SCREEN_WIDTH
+        if self.y <= 0:
+            self.y = SCREEN_HEIGHT
+        elif self.y >= SCREEN_HEIGHT:
+            self.y = 0
+        return self.x, self.y
+
 
 class APPLE(object):
     def __init__(self):
-        random_x_pos = random.randint(0,SCREEN_WIDTH)
-        random_y_pos = random.randint(0,SCREEN_HEIGHT)
+        random_x_pos = random.randint(0, SCREEN_WIDTH)
+        random_y_pos = random.randint(0, SCREEN_HEIGHT)
         self.x = random_x_pos
         self.y = random_y_pos
-        WIDTH = 10
-        HEIGHT = 10
+        WIDTH = 100 / 4
+        HEIGHT = 100 / 4
+        self.radius = 20
         self.rect = pygame.rect.Rect((self.x, self.y, WIDTH, HEIGHT))
+
     def draw(self):
-        pygame.draw.rect(screen, COLOR.WHITE, self.rect)           
+        pygame.draw.circle(screen, COLOR.RED, (self.x, self.y), self.radius)
+
+
+# Set timer for apple
+spawn_apple = pygame.USEREVENT
+spawn_apple_time = 1200
+pygame.time.set_timer(spawn_apple, spawn_apple_time)
 
 # Object init
 world = WORLD()
 snake = SNAKE()
 apple = APPLE()
+
+
 def main():
     while world.game_active:
         screen.fill(COLOR.BLACK)
@@ -69,13 +101,26 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 sys.exit()
-            if event.type == pygame.KEYDOWN:
+            if event.type == pygame.KEYDOWN:    
                 if event.key == pygame.K_ESCAPE:
                     world.game_active = False
-        snake.draw()
-        snake.handle_keys()
+                if event.key == pygame.K_RIGHT:
+                    world.direction = 'RIGHT'
+                if event.key == pygame.K_LEFT:
+                    world.direction = 'LEFT'
+                if event.key == pygame.K_DOWN:
+                    world.direction = 'DOWN'
+                if event.key == pygame.K_UP:
+                    world.direction = 'UP'
+
+        snake.x, snake.y = snake.change_direction()
+        snake.x, snake.y = snake.reset_snake()        
+        snake.draw(snake.x, snake.y)
 
         apple.draw()
+
+        if apple.rect.colliderect(snake.rect):
+            print('touch')
 
         pygame.display.update()
         world.clock.tick(world.fps_number)

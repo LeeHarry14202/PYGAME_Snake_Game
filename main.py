7e6e50d63968a6678e67440bec8e92b8eef92f04
LeeHarry14202 <20521912@gm.uit.edu.vn>
2021-10-02 17:21:18 +0700

    Update main.py

--- main.py
+++ main.py
@@ -27,13 +27,20 @@ class WORLD(pygame.sprite.Sprite):
         # Set direction
         self.direction = 'RIGHT'
 
-    def restart(self):
+    def reset(self):
+
+        world.direction = 'RIGHT'
         world.game_active = True
+
         apple.list_apple.clear()
+
         snake.x = SCREEN_WIDTH / 2
         snake.y = SCREEN_HEIGHT / 2
-        world.direction = 'RIGHT'
-        world.score = 0
+        snake.len_list_snake = 0
+        snake.list_snake = [[SCREEN_WIDTH/2, SCREEN_HEIGHT/2]]
+
+        score.score_ = 0
+        score.start_time = 100
 
     def display_text(self, text, x, y):
         # creating font object my_font
@@ -58,12 +65,17 @@ class SCORE(object):
     def __init__(self):
         self.score_ = 0
         self.high_score = 0
+        self.start_time = 100
 
     def display_score(self):
-        world.display_text('Your score: ' + str(self.score_), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4)
-        world.display_text('Press SPACE to play again', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2.5)
-        world.display_text('Press ESC to exit', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)        
-        world.display_text('High score: ' + str(self.high_score), SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100)
+        if score.start_time != 0:
+            world.display_text('YOU HIT THE WALL!', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+            score.start_time -= 1
+        else:
+            world.display_text('Your score: ' + str(self.score_), SCREEN_WIDTH / 2, SCREEN_HEIGHT / 4)
+            world.display_text('Press SPACE to play again', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2.5)
+            world.display_text('Press ESC to exit', SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)        
+            world.display_text('High score: ' + str(self.high_score), SCREEN_WIDTH / 2, SCREEN_HEIGHT - 100)
 
     def update_high_score(self):
         if self.high_score < self.score_:
@@ -77,33 +89,63 @@ class SNAKE(object):
         self.WIDTH = 20
         self.HEIGHT = 20
         self.speed = 3
-        self.list_snake = []
-        self.position = [self.x, self.y]
+        self.list_snake = [[self.x, self.y]]
+        self.len_list_snake = 0
         self.rect = pygame.rect.Rect((self.x, self.y, self.WIDTH, self.HEIGHT))
-
+    
     def draw(self):
-        self.rect = pygame.rect.Rect((self.x, self.y, self.WIDTH, self.HEIGHT))
-        pygame.draw.rect(screen, COLOR.WHITE, self.rect)
+        # Press to change snake direction
+        self.change_direction()
+
+        self.add_to_list_snake()
+
+        for snake_ in self.list_snake:
+            self.x = snake_[0]
+            self.y = snake_[1]
+            pygame.draw.rect(screen, COLOR.WHITE, [self.x, self.y, self.WIDTH, self.HEIGHT])
 
     def change_direction(self):
+        x_change = 0
+        y_change = 0
         if world.direction == 'RIGHT':     
-            self.x += self.speed
+            x_change = self.speed
+            y_change = 0
         elif world.direction == 'LEFT':
-            self.x -= self.speed
+            x_change = - self.speed
+            y_change = 0
         elif world.direction == 'UP':
-            self.y -= self.speed
+            x_change = 0
+            y_change = - self.speed
         elif world.direction == 'DOWN':
-            self.y += self.speed
+            x_change = 0
+            y_change = self.speed
+        self.x += x_change
+        self.y += y_change
+        # return x, y
+
+    def add_to_list_snake(self):
+        head = []
+        head.append(snake.x)
+        head.append(snake.y)
+        snake.list_snake.append(head)
+        if len(snake.list_snake) > snake.len_list_snake:
+            del snake.list_snake[0]
 
     def hit_wall(self):
         if self.x > SCREEN_WIDTH:
             world.game_active = False
         elif self.x < 0:
             world.game_active = False
-        if self.y < 0:
+        elif self.y < 0:
             world.game_active = False
         elif self.y > SCREEN_HEIGHT:
             world.game_active = False
+
+    def check_collision(self):
+        for snake_ in snake.list_snake:
+            snake_rect = pygame.rect.Rect((snake_[0], snake_[1], self.WIDTH, self.HEIGHT))
+            if snake_rect.colliderect(apple.rect):
+                score.score_ += 1 
 
 
 class APPLE(object):
@@ -153,10 +195,10 @@ spawn_apple = pygame.USEREVENT
 spawn_apple_time = 2000
 pygame.time.set_timer(spawn_apple, spawn_apple_time)
 
-# # Set timer for 
-# spawn_apple = pygame.USEREVENT
-# spawn_apple_time = 2000
-# pygame.time.set_timer(spawn_apple, spawn_apple_time)
+# # # Set timer for 
+spawn_snake = pygame.USEREVENT + 1
+spawn_snake_time = 1000
+pygame.time.set_timer(spawn_snake, spawn_snake_time)
 
 # Object init
 world = WORLD()
@@ -168,6 +210,8 @@ score = SCORE()
 
 def main():
     running = True
+    # x_change = 0
+    # y_change = 0
     while running:
         screen.fill(COLOR.BLACK)
         for event in pygame.event.get():
@@ -187,21 +231,21 @@ def main():
                 else:
                     # Press SPACE to restart
                     if event.key == pygame.K_SPACE:
-                        world.restart()
+                        world.reset()
                     # Press Esc to exit
                     if event.key == pygame.K_ESCAPE:
                         running = False
             # To create list of apple
             if event.type == spawn_apple:
                 apple.list_apple.append(apple.create_apple())
+            if event.type == spawn_snake and world.game_active:
+                snake.len_list_snake += 1
 
         # Draw wall around 
         wall.draw()
 
         if world.game_active:
-            # Find snake direction
-            snake.change_direction()
-       
+
             # Draw snake
             snake.draw()
 
@@ -209,8 +253,7 @@ def main():
             apple.draw(apple.list_apple)
 
             # Check collision
-            if apple.rect.colliderect(snake.rect):
-                score.score_ += 1
+            snake.check_collision()
 
             # Check if the snake hit the wall, game over
             snake.hit_wall()
